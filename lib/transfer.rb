ea18d1aabd599391c5c7a615e9b62d00198f4925
Ashleigh Chun <ashleigh.chun@wework.com>
2018-12-12 19:43:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  
+  def initialize(sender, receiver, amount = 50)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end 
+  
+  def valid?
+    bank_account.status = "open"
+  end 
+  
+  def execute_transaction
+    
+  end 
+  
+  def reverse_transaction
+    
+  end 
+  
+  def reverse_transfer
+    
+  end 
 end
