3452eef7b07e81749e4cf90912f72ddd9ffb8467
Ashleigh Chun <ashleigh.chun@wework.com>
2018-12-13 01:43:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,32 @@
+require "pry"
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount, :bankaccount
+  
+  
+  def initialize(sender, receiver, amount = 50)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+    @bankaccount = bankaccount
+  end 
+  
+  def valid?
+    @bankaccount = BankAccount.valid?
+    @amount > 0 && @bankaccount == true
+    binding.pry
+  end 
+  
+  def execute_transaction
+    
+  end 
+  
+  def reverse_transaction
+    
+  end 
+  
+  def reverse_transfer
+    
+  end 
 end
